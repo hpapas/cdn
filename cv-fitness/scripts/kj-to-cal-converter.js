ea1c8c1d2c1188b37760966324f50e7aa8dbf27e
hpapas <109855242+hpapas@users.noreply.github.com>
2023-11-09 22:09:37 +1030

    Update kj-to-cal-converter.js

--- cv-fitness/scripts/kj-to-cal-converter.js
+++ cv-fitness/scripts/kj-to-cal-converter.js
@@ -1,39 +1,26 @@
 <script>
-document.addEventListener('DOMContentLoaded', function () {
-  function convert(source) {
-    const kJInput = document.getElementById('kJ');
-    const calInput = document.getElementById('cal');
-    const resultDiv = document.getElementById('result');
+    function convert(source) {
+      const kJInput = document.getElementById('kJ');
+      const calInput = document.getElementById('cal');
+      const resultDiv = document.getElementById('result');
 
-    const kJValue = parseFloat(kJInput.value);
-    const calValue = parseFloat(calInput.value);
+      const kJValue = parseFloat(kJInput.value);
+      const calValue = parseFloat(calInput.value);
 
-    if (source === 'kJ' && !isNaN(kJValue)) {
-      // Convert kJ to cal (1 kJ ≈ 0.239 kcal)
-      const convertedValue = kJValue * 0.239;
-      calInput.value = convertedValue.toFixed(2);
-      resultDiv.innerHTML = '';
-    } else if (source === 'cal' && !isNaN(calValue)) {
-      // Convert cal to kJ (1 kcal ≈ 4.184 kJ)
-      const convertedValue = calValue / 4.184;
-      kJInput.value = convertedValue.toFixed(2);
-      resultDiv.innerHTML = '';
-    } else {
-      // Handle invalid input
-      resultDiv.innerHTML = 'Please enter a valid number.';
+      if (source === 'kJ' && !isNaN(kJValue)) {
+        // Convert kJ to cal (1 kJ ≈ 0.239 kcal)
+        const convertedValue = kJValue * 0.239;
+        calInput.value = convertedValue.toFixed(2);
+        resultDiv.innerHTML = '';
+      } else if (source === 'cal' && !isNaN(calValue)) {
+        // Convert cal to kJ (1 kcal ≈ 4.184 kJ)
+        const convertedValue = calValue / 4.184;
+        kJInput.value = convertedValue.toFixed(2);
+        resultDiv.innerHTML = '';
+      } else {
+        // Handle invalid input
+        resultDiv.innerHTML = 'Please enter a valid number.';
+      }
     }
-  }
-
-  const kJInput = document.getElementById('kJ');
-  const calInput = document.getElementById('cal');
-
-  kJInput.addEventListener('input', function () {
-    convert('kJ');
-  });
-
-  calInput.addEventListener('input', function () {
-    convert('cal');
-  });
-});
 
   </script>
