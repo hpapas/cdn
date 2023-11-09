ccf69743fe8260b3e52e02879e53b34e31f3aadd
harrypapas96 <109855242+harrypapas96@users.noreply.github.com>
2023-11-09 20:47:02 +1030

    Create kj-to-cal-converter.js

---
+++ cv-fitness/scripts/kj-to-cal-converter.js
@@ -0,0 +1,25 @@
+<script>
+    function convert(source) {
+      const kJInput = document.getElementById('kJ');
+      const calInput = document.getElementById('cal');
+      const resultDiv = document.getElementById('result');
+
+      const kJValue = parseFloat(kJInput.value);
+      const calValue = parseFloat(calInput.value);
+
+      if (source === 'kJ' && !isNaN(kJValue)) {
+        // Convert kJ to cal (1 kJ ≈ 0.239 kcal)
+        const convertedValue = kJValue * 0.239;
+        calInput.value = convertedValue.toFixed(2);
+        resultDiv.innerHTML = '';
+      } else if (source === 'cal' && !isNaN(calValue)) {
+        // Convert cal to kJ (1 kcal ≈ 4.184 kJ)
+        const convertedValue = calValue / 4.184;
+        kJInput.value = convertedValue.toFixed(2);
+        resultDiv.innerHTML = '';
+      } else {
+        // Handle invalid input
+        resultDiv.innerHTML = 'Please enter a valid number.';
+      }
+    }
+  </script>
