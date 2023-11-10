061dfea652634fd38e39a493f947c99ea499c5bc
hpapas <109855242+hpapas@users.noreply.github.com>
2023-11-10 20:30:46 +1030

    Update calorie-calculator.js

--- cv-fitness/scripts/calorie-calculator.js
+++ cv-fitness/scripts/calorie-calculator.js
@@ -20,6 +20,7 @@ function calculateCalories() {
     var height = parseFloat($('#height').val());
     var activityLevel = $('#activity').val();
     var units = $('#units').val(); // Get selected units
+    var goal = $('#goal').val(); // Get selected goal
 
     // Convert weight and height to metric or imperial if necessary
     if (units === 'imperial') {
@@ -28,7 +29,7 @@ function calculateCalories() {
     }
 
     // Perform the calculation using Harris-Benedict Formula
-    var calculatedCalories = calculateDailyCalories(age, weight, height, activityLevel);
+    var calculatedCalories = calculateDailyCalories(age, weight, height, activityLevel, goal);
 
     // Check if the result is a valid number
     if (!isNaN(calculatedCalories)) {
@@ -41,32 +42,27 @@ function calculateCalories() {
 }
 
 // Function to calculate BMR using Harris-Benedict Formula
-function calculateDailyCalories(age, weight, height, activityLevel) {
+function calculateDailyCalories(age, weight, height, activityLevel, goal) {
     // Perform Harris-Benedict Formula calculation
     var baseCalories = 10 * weight + 6.25 * height - 5 * age;
     var activityMultiplier = getActivityMultiplier(activityLevel);
     var calculatedCalories = baseCalories * activityMultiplier;
 
-    return calculatedCalories;
-}
-
-// Function to get the activity level multiplier
-function getActivityMultiplier(activityLevel) {
-    // Replace with your actual logic to determine the activity multiplier
-    switch (activityLevel) {
-        case 'sedentary':
-            return 1.2;
-        case 'light':
-            return 1.375;
-        case 'moderate':
-            return 1.55;
-        case 'active':
-            return 1.725;
-        case 'veryActive':
-            return 1.9;
-        default:
-            return 1.2; // Default to sedentary if activity level is not recognized
+    // Adjust calories based on the goal
+    switch (goal) {
+        case 'lose':
+            calculatedCalories -= 500; // Adjust for weight loss (example value)
+            break;
+        case 'maintain':
+            // No adjustment for weight maintenance
+            break;
+        case 'gain':
+            calculatedCalories += 300; // Adjust for weight gain (example value)
+            break;
+        // Add more cases for other goals if needed
     }
+
+    return calculatedCalories;
 }
 
 // Wait for the document to be ready
@@ -82,13 +78,6 @@ $(document).ready(function () {
 
     // Attach a click event listener to the "Calculate" button
     $('#calculate').on('click', function () {
-        // Get the value of the first email field
-        var value1 = $('#email').val();
-
-        // Automatically fill the second email field with the value of the first one
-        $('#email-yui_3_17_2_1_1699523189995_1516-field').val(value1);
-
-        // Trigger the form submission for the Squarespace newsletter block
-        $('#email-yui_3_17_2_1_1699523189995_1516-field').submit();
+        calculateCalories(); // Call the calculateCalories function
     });
 });
