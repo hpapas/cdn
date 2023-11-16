3229b5a19611f9297d33e0ac66c4c81d12ebd353
hpapas <109855242+hpapas@users.noreply.github.com>
2023-11-16 22:01:43 +1030

    Delete cv-fitness/scripts/core.js

--- cv-fitness/scripts/core.js
+++
@@ -1,9 +0,0 @@
-function callAll(jsfiles) {
-    var src = document.createElement("script");
-    src.setAttribute("type", "text/javascript");
-    src.setAttribute("src", jsfiles);
-    document.getElementsByTagName("head")[0].appendChild(src);
-}
-
-callAll("https://cdn.jsdelivr.net/gh/willmyethewebsiteguy/beforeAfter@1/javascript.min.js");
-callAll("https://cdn.jsdelivr.net/gh/willmyethewebsiteguy/tabs@4/tabs-javascript.min.js");
