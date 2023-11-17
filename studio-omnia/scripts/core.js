85cdb6a7cc74b592bf429b5d51f9865b6cc055cc
hpapas <109855242+hpapas@users.noreply.github.com>
2023-11-17 20:40:33 +1030

    Create core.js

---
+++ studio-omnia/scripts/core.js
@@ -0,0 +1,19 @@
+var year = new Date().getFullYear();
+
+jQuery(document).ready(function() {
+  
+  if (window.location.href.indexOf("gallery") > -1) {
+    jQuery(".header-nav-item--external").css({"display":"none"});
+  }
+  
+  if (window.location.href.indexOf("terms-conditions") > -1) {
+    jQuery(".header-nav-item--external").css({"display":"none"});
+  }
+  
+  if (window.location.href.indexOf("book") > -1) {
+    jQuery(".header-nav-item--external").css({"display":"none"});
+  }
+
+  $('#block-a96f5105d48ffeb6c8fb p').html($('#block-a96f5105d48ffeb6c8fb p').html() .replace('[Year]', year));
+
+});
