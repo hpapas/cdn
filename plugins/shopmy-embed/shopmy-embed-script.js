078d67bc9542c470678ea252eeffd573e3800b46
hpapas <109855242+hpapas@users.noreply.github.com>
2024-07-06 16:42:25 +0930

    Update shopmy-embed-script.js

--- plugins/shopmy-embed/shopmy-embed-script.js
+++ plugins/shopmy-embed/shopmy-embed-script.js
@@ -1,18 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var shopmyDivs = document.querySelectorAll('[data-hp-shopmy-collectionid]');
-    
-//     shopmyDivs.forEach(function(shopmyDiv) {
-//         var collectionId = shopmyDiv.getAttribute('data-hp-shopmy-collectionid');
-//         var collectionTitle = shopmyDiv.getAttribute('data-hp-shopmy-title');
-        
-//         var embedCode = '<iframe title="' + collectionTitle + '" src="https://shopmy.us/collections/embed/' + collectionId + '?" style="width: 100%; min-height: 360px; border: none;"></iframe>';
-
-//         // Replace the content of the div with the generated embed code
-//         shopmyDiv.innerHTML = embedCode;
-//     });
-// });
-
-
 document.addEventListener("DOMContentLoaded", function() {
     var shopmyDivs = document.querySelectorAll('[data-hp-shopmy-collectionid]');
     
