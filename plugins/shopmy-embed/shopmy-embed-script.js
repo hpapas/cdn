5752b030036db71c89d0bdf5ed82dcb8190e2824
hpapas <109855242+hpapas@users.noreply.github.com>
2024-07-06 16:22:41 +0930

    Update shopmy-embed-script.js

--- plugins/shopmy-embed/shopmy-embed-script.js
+++ plugins/shopmy-embed/shopmy-embed-script.js
@@ -1,11 +1,36 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     var shopmyDivs = document.querySelectorAll('[data-hp-shopmy-collectionid]');
+    
+//     shopmyDivs.forEach(function(shopmyDiv) {
+//         var collectionId = shopmyDiv.getAttribute('data-hp-shopmy-collectionid');
+//         var collectionTitle = shopmyDiv.getAttribute('data-hp-shopmy-title');
+        
+//         var embedCode = '<iframe title="' + collectionTitle + '" src="https://shopmy.us/collections/embed/' + collectionId + '?" style="width: 100%; min-height: 360px; border: none;"></iframe>';
+
+//         // Replace the content of the div with the generated embed code
+//         shopmyDiv.innerHTML = embedCode;
+//     });
+// });
+
+
 document.addEventListener("DOMContentLoaded", function() {
     var shopmyDivs = document.querySelectorAll('[data-hp-shopmy-collectionid]');
     
     shopmyDivs.forEach(function(shopmyDiv) {
         var collectionId = shopmyDiv.getAttribute('data-hp-shopmy-collectionid');
         var collectionTitle = shopmyDiv.getAttribute('data-hp-shopmy-title');
+        var embedType = shopmyDiv.getAttribute('data-hp-shopmy-embed') || 'carousel'; // Default to 'carousel' if not specified
         
-        var embedCode = '<iframe title="' + collectionTitle + '" src="https://shopmy.us/collections/embed/' + collectionId + '?" style="width: 100%; min-height: 360px; border: none;"></iframe>';
+        var src = 'https://shopmy.us/collections/embed/' + collectionId + '?';
+        var style = 'width: 100%; min-height: 360px; border: none;';
+
+        // Check embed type
+        if (embedType === 'full') {
+            src = 'https://shopmy.us/collections/collection/' + collectionId + '?noHeader=true';
+            style = 'width: 100%; min-height: 1200px; border: none;';
+        }
+
+        var embedCode = '<iframe title="' + collectionTitle + '" src="' + src + '" style="' + style + '"></iframe>';
 
         // Replace the content of the div with the generated embed code
         shopmyDiv.innerHTML = embedCode;
