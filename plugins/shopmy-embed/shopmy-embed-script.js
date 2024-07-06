52f16192b28f128f5f24418810536899781bb4ad
hpapas <109855242+hpapas@users.noreply.github.com>
2024-07-06 16:29:20 +0930

    Update shopmy-embed-script.js

--- plugins/shopmy-embed/shopmy-embed-script.js
+++ plugins/shopmy-embed/shopmy-embed-script.js
@@ -19,17 +19,26 @@ document.addEventListener("DOMContentLoaded", function() {
     shopmyDivs.forEach(function(shopmyDiv) {
         var collectionId = shopmyDiv.getAttribute('data-hp-shopmy-collectionid');
         var collectionTitle = shopmyDiv.getAttribute('data-hp-shopmy-title');
-        var embedType = shopmyDiv.getAttribute('data-hp-shopmy-embed') || 'carousel'; // Default to 'carousel' if not specified
+        var embedType = shopmyDiv.getAttribute('data-hp-shopmy-embed-type') || 'carousel'; // Default to 'carousel' if not specified
         
         var src, style;
 
         if (embedType === 'full') {
             src = 'https://shopmy.us/collections/collection/' + collectionId + '?noHeader=true';
             style = 'width: 100%; min-height: 1200px; border: none;';
-        } else {
+        } else if (embedType === 'collection') {
             src = 'https://shopmy.us/collections/embed/' + collectionId + '?';
             style = 'width: 100%; min-height: 360px; border: none;';
+        } else {
+            // Handle any other types or provide a default behavior if needed
+            console.warn("Unknown embed type: ", embedType);
+            return;
         }
+
+        // Debug output
+        console.log("Embed Type:", embedType);
+        console.log("Iframe SRC:", src);
+        console.log("Iframe Style:", style);
 
         var embedCode = '<iframe title="' + collectionTitle + '" src="' + src + '" style="' + style + '"></iframe>';
         
@@ -37,4 +46,3 @@ document.addEventListener("DOMContentLoaded", function() {
         shopmyDiv.innerHTML = embedCode;
     });
 });
-
