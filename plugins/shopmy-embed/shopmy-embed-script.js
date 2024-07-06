0c572a2500a7214561bff9e91b25daf90593c219
hpapas <109855242+hpapas@users.noreply.github.com>
2024-07-06 16:26:21 +0930

    Update shopmy-embed-script.js

--- plugins/shopmy-embed/shopmy-embed-script.js
+++ plugins/shopmy-embed/shopmy-embed-script.js
@@ -21,18 +21,20 @@ document.addEventListener("DOMContentLoaded", function() {
         var collectionTitle = shopmyDiv.getAttribute('data-hp-shopmy-title');
         var embedType = shopmyDiv.getAttribute('data-hp-shopmy-embed') || 'carousel'; // Default to 'carousel' if not specified
         
-        var src = 'https://shopmy.us/collections/embed/' + collectionId + '?';
-        var style = 'width: 100%; min-height: 360px; border: none;';
+        var src, style;
 
-        // Check embed type
         if (embedType === 'full') {
             src = 'https://shopmy.us/collections/collection/' + collectionId + '?noHeader=true';
             style = 'width: 100%; min-height: 1200px; border: none;';
+        } else {
+            src = 'https://shopmy.us/collections/embed/' + collectionId + '?';
+            style = 'width: 100%; min-height: 360px; border: none;';
         }
 
         var embedCode = '<iframe title="' + collectionTitle + '" src="' + src + '" style="' + style + '"></iframe>';
-
+        
         // Replace the content of the div with the generated embed code
         shopmyDiv.innerHTML = embedCode;
     });
 });
+
