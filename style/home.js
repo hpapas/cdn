b6b782d0fa280ea3b4f195182fdc8dd9d67934eb
hpapas <109855242+hpapas@users.noreply.github.com>
2024-08-02 20:33:47 +0930

    Create home.js

---
+++ style/home.js
@@ -0,0 +1,10 @@
+<script>
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    var availability = "true"; // Change this to "false" as needed
+    var indicator = document.getElementById("availability-indicator");
+    
+    indicator.setAttribute("data-availability", availability);
+  });
+
+</script>
