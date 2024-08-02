6e52f7c955290ac70295874d0db58a475c74a9ae
hpapas <109855242+hpapas@users.noreply.github.com>
2024-08-02 20:34:23 +0930

    Update home.js

--- style/home.js
+++ style/home.js
@@ -1,10 +1,6 @@
-<script>
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    var availability = "true"; // Change this to "false" as needed
-    var indicator = document.getElementById("availability-indicator");
+document.addEventListener("DOMContentLoaded", function() {
+  var availability = "true"; // Change this to "false" as needed
+  var indicator = document.getElementById("availability-indicator");
     
-    indicator.setAttribute("data-availability", availability);
-  });
-
-</script>
+  indicator.setAttribute("data-availability", availability);
+});
