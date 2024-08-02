f5376114987d080bd0e42dc35deda7bed1acb90b
hpapas <109855242+hpapas@users.noreply.github.com>
2024-08-02 21:08:16 +0930

    Update home.js

--- style/home.js
+++ style/home.js
@@ -5,10 +5,10 @@
 //   indicator.setAttribute("data-availability", availability);
 // });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const indicator = document.getElementById('availability-indicator');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const indicator = document.getElementById('availability-indicator');
   
-  // Update the availability based on your logic
-  const isAvailable = true; // or false based on your condition
-  indicator.setAttribute('data-availability', isAvailable ? 'true' : 'false');
-});
+//   // Update the availability based on your logic
+//   const isAvailable = true; // or false based on your condition
+//   indicator.setAttribute('data-availability', isAvailable ? 'true' : 'false');
+// });
