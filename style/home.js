0f2916928d08a13aaeb84f3d4a44db8c7d6103b6
hpapas <109855242+hpapas@users.noreply.github.com>
2024-08-02 21:03:09 +0930

    Update home.js

--- style/home.js
+++ style/home.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var availability = "true"; // Change this to "false" as needed
+  var availability = "false"; // Change this to "false" as needed
   var indicator = document.getElementById("availability-indicator");
     
   indicator.setAttribute("data-availability", availability);
