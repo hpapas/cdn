93027175607aa8435ef28272619e60d4354f8639
hpapas <109855242+hpapas@users.noreply.github.com>
2024-08-03 18:08:38 +0930

    Delete files/script.js

--- files/script.js
+++
@@ -1,4 +0,0 @@
-<!--- ANIMATED SCROLLING LOGO CAROUSEL --->
-<script>
-  document.addEventListener("DOMContentLoaded", function() {const scrollers = document.querySelectorAll('.user-items-list[data-space-below-section-title-value="92"]');scrollers.forEach((scroller) => {scroller.setAttribute("data-animated", true);if (scroller.querySelector('.list-item[data-is-card-enabled="true"]')) { scroller.closest('.content').classList.add('logo-gradient');}const scrollerInner = scroller.querySelector(".user-items-list-simple");const scrollerContent = Array.from(scrollerInner.children);scrollerContent.forEach((item) => {const duplicatedItem = item.cloneNode(true);duplicatedItem.setAttribute("aria-hidden", true);scrollerInner.appendChild(duplicatedItem);});const images = scroller.querySelectorAll('img');images.forEach((img)=>{const imgSrc=img.getAttribute('data-src');if(imgSrc){ img.parentElement.style.backgroundImage = `url('${imgSrc}')`;}});});});
-</script>
